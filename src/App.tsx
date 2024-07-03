d20aa6a9e1d7b6c4facb2d3869c95c0c2b24f857
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-07-03 16:09:10 -0500

    pages and routes boilerplate added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,71 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "product/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
   return (
     <>
-      <h1 className="text-red-500">Hello</h1>
+      <RouterProvider router={router} />
     </>
   );
 }

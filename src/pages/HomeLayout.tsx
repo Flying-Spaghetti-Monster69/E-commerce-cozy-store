4432ff6d0e2494f1025ce72cb5981997892b2004
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-07-03 19:26:29 -0500

    added custom class align-element

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -6,7 +6,9 @@ const HomeLayout = () => {
       <nav>
         <span className="text-4xl text-primary">Cozy</span>
       </nav>
-      <Outlet />
+      <section className="align-element py-20">
+        <Outlet />
+      </section>
     </>
   );
 };

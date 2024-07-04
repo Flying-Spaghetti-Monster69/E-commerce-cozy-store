9ab5ec5ccecc192880271167e3709e60f70fa598
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-07-03 19:08:08 -0500

    login added and submit refactored

--- src/components/SubmitBtn.tsx
+++ src/components/SubmitBtn.tsx
@@ -1,10 +1,19 @@
-interface Props {
-  text: string;
-}
-const SubmitBtn = ({ text }: Props) => {
+import { useNavigation } from "react-router-dom";
+
+const SubmitBtn = ({ text = "submit" }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
-    <button type="submit" className="btn btn-primary">
-      {text}
+    <button
+      type="submit"
+      className="btn btn-primary btn-block"
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <span className="loading loading-dots">sending...</span>
+      ) : (
+        text
+      )}
     </button>
   );
 };

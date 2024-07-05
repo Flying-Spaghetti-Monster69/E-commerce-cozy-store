9667153674eb893b4666b35cfe19f93911fe427e
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-07-05 10:15:49 -0500

    css associates slider bugs fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       keyframes: {
         "loop-scroll": {
           from: { transform: "translateX(0)" },
-          to: { transform: "translateX(-100%)" },
+          to: { transform: "translateX(-104.45%)" },
         },
       },
     },

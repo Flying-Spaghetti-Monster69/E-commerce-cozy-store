4fd59e015ac996585c075bdc5fee490f55f1d792
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-07-07 14:06:17 -0500

    cart store placeholder added

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -5,7 +5,39 @@ type themeStore = {
   setStoreTheme: (theme: string) => void;
 };
 
+type cartStore = {
+  cartItems: [];
+  numItemsInCart: number;
+  cartTotal: number;
+  shipping: number;
+  tax: number;
+  orderTotal: number;
+  addItem: (cartItem: any) => void;
+  clearCart: () => void;
+  removeItem: (id: any) => void;
+};
+
 export const useThemeStore = create<themeStore>((set) => ({
   theme: "nord",
   setStoreTheme: (theme) => set({ theme }),
+}));
+
+export const useCartStore = create<cartStore>((set) => ({
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+  addItem: (cartItem) => console.log(cartItem),
+  clearCart: () =>
+    set({
+      cartItems: [],
+      numItemsInCart: 0,
+      cartTotal: 0,
+      shipping: 500,
+      tax: 0,
+      orderTotal: 0,
+    }),
+  removeItem: (id) => console.log(id),
 }));

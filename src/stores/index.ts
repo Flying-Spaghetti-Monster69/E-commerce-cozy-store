115688f74e9be457259101a62fdb9cc295e0c489
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-07-07 18:30:07 -0500

    store initial logic added

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -11,7 +11,8 @@ interface themeStore {
 interface cartStore extends cart {
   addItem: (cartItemToAdd: cartProduct) => void;
   clearCart: () => void;
-  removeItem: (id: any) => void;
+  removeItem: (id: string) => void;
+  editItem: (id: string, amount: number) => void;
   getItemsFromLocalStorage: () => void;
   calculateTotals: () => void;
 }
@@ -50,7 +51,18 @@ export const useCartStore = create<cartStore>((set, get) => ({
 
     get().calculateTotals();
   },
-  clearCart: () =>
+  clearCart: () => {
+    localStorage.setItem(
+      LOCAL_STORAGE_CART,
+      JSON.stringify({
+        cartItems: [],
+        numItemsInCart: 0,
+        cartTotal: 0,
+        shipping: 500,
+        tax: 0,
+        orderTotal: 0,
+      })
+    );
     set({
       cartItems: [],
       numItemsInCart: 0,
@@ -58,8 +70,39 @@ export const useCartStore = create<cartStore>((set, get) => ({
       shipping: 500,
       tax: 0,
       orderTotal: 0,
-    }),
-  removeItem: (id) => console.log(id),
+    });
+  },
+  removeItem: (id) => {
+    const currentItems = get().cartItems;
+    const product = currentItems.find((cartItem) => cartItem.CartID === id);
+    if (product) {
+      set({
+        cartItems: currentItems.filter((cartItem) => cartItem.CartID !== id),
+      });
+      set((state) => ({
+        numItemsInCart: state.numItemsInCart - product.amount,
+        cartTotal: state.cartTotal - Number(product.price) * product.amount,
+      }));
+      get().calculateTotals();
+    }
+  },
+  editItem(id, amount) {
+    const currentItems = get().cartItems;
+    const product = currentItems.findIndex(
+      (cartItem) => cartItem.CartID === id
+    );
+    if (product !== -1) {
+      set((state) => ({
+        numItemsInCart:
+          state.numItemsInCart + (amount - currentItems[product].amount),
+        cartTotal:
+          state.cartTotal -
+          Number(currentItems[product].price) *
+            (amount - currentItems[product].amount),
+      }));
+    }
+    get().calculateTotals();
+  },
   getItemsFromLocalStorage: () => {
     const cart = localStorage.getItem(LOCAL_STORAGE_CART);
     if (cart) {

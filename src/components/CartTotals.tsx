ceab9cf5570420e054a20d56f2d630566b54c1e0
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2024-07-07 19:14:57 -0500

    cartTotals component added

--- src/components/CartTotals.tsx
+++ src/components/CartTotals.tsx
@@ -1,4 +1,31 @@
+import { useCartStore } from "../stores";
+import { formatPrice } from "../utils";
+
 const CartTotals = () => {
-  return <h1 className="text-3xl">CartTotals</h1>;
+  const { cartTotal, shipping, tax, orderTotal } = useCartStore();
+  return (
+    <div className="card bg-base-200">
+      <div className="card-body">
+        <p className="flex justify-between text-xs border-b border-base-300">
+          <span>Subtotal</span>
+          <span className="font-bold">{formatPrice(cartTotal.toString())}</span>
+        </p>
+        <p className="flex justify-between text-xs border-b border-base-300">
+          <span>tax</span>
+          <span className="font-bold">{formatPrice(tax.toString())}</span>
+        </p>
+        <p className="flex justify-between text-xs border-b border-base-300">
+          <span>shipping</span>
+          <span className="font-bold">{formatPrice(shipping.toString())}</span>
+        </p>
+        <p className="flex justify-between text-sm mt-4">
+          <span>Order total</span>
+          <span className="font-bold">
+            {formatPrice(orderTotal.toString())}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 };
 export default CartTotals;
